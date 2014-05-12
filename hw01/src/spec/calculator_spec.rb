71ec60b66e619d8ed32e75d8c3c7514429095ca8
Francis Brito <fr.br94@gmail.com>
2014-05-11 23:42:55 -0400

    Add test for invalid arguments.

--- hw01/src/spec/calculator_spec.rb
+++ hw01/src/spec/calculator_spec.rb
@@ -1,8 +1,11 @@
-require 'spec_Helper'
+require 'spec_helper'
 
 class Calculator
   def add num_one, num_two
-    num_one +  num_two
+    # `obj.is_a? Numeric` returns `true` for both Integer and Float.
+    raise ArgumentError.new "Invalid argument '#{num_one}'." if not num_one.is_a? Numeric or not num_two.is_a? Numeric
+
+    num_one + num_two
   end
 end
 
@@ -17,7 +20,21 @@ describe Calculator do
     end
 
     context "with incorrect params" do
-      it "should throw an error"
+      it "should throw an error" do
+        calc = Calculator.new
+
+        # TODO: Think of other invalid arguments besides strings.
+        # NOTE: Using a scope is required, since RSpec runs the bit of code
+        #       passed to `expect`, without it `calc.add 'invalid', 'params'`
+        #       would throw breaking the test.
+        #
+        #       This is analogous to doing
+        #       ```js
+        #       expect(function() { obj.funcThatThrows(); }).toThrow();
+        #       ```
+        #       in Mocha/Jasmine.
+        expect { calc.add 'invalid', 'params' }.to raise_error(ArgumentError)
+      end
     end
   end
 
