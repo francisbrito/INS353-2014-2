a0eb1b7fe659214b557555f88a4b82ba14bcf067
Francis Brito <fr.br94@gmail.com>
2014-05-15 16:02:59 +0000

    All red

--- class01/src/spec/triangle_spec.rb
+++ class01/src/spec/triangle_spec.rb
@@ -1,19 +1,52 @@
 require 'spec_helper'
 
+class ViolatesTriangleInequalityError < TypeError
+end
+
+class ImpossibleLengthError < TypeError
+end
+
+class InvalidTypeEror < TypeError
+end
+
 class Triangle
 end
 
 describe Triangle do
   describe '#kind' do
     context 'with valid sides' do
-      it 'should return `equilateral` if all sides are equal.'
-      it 'should return `isosceles` if two sides are equal.'
-      it 'should return `scalene` if all sides are different.'
+      it 'should return `equilateral` if all sides are equal.' do
+        triangle = Triangle.new 1, 1, 1
+        
+        expect(triangle.kind).to eql(:equilateral)
+      end
+      it 'should return `isosceles` if two sides are equal.' do
+        triangle = Triangle.new 1, 1, 2
+        
+        expect(triangle.kind).to eql(:isosceles)
+      end
+      it 'should return `scalene` if all sides are different.' do
+        triangle = Triangle.new 1, 2, 3
+        
+        expect(triangle.kind).to eql(:scalene)
+      end
     end
     context 'with invalid sides' do
-      it 'should throw `ViolatesTriangleInequalityError` if a third size is greater than the sum of the first two.'
-      it 'should throw `ImpossibleSideLengthError` if any of the sides is zero or negative.'
-      it 'should throw `InvalidTypeError` if any of the of sides is not a number.'
+      it 'should throw `ViolatesTriangleInequalityError` if a third side is greater than the sum of the first two.' do
+        triangle = Triangle.new 1, 2, 4
+
+        expect {triangle.kind}.to raise_error(ViolatesTriangleInequalityError)
+      end
+      it 'should throw `ImpossibleSideLengthError` if any of the sides is zero or negative.' do
+        triangle = Triangle.new 1, -1, 0
+        
+        expect {triangle.kind}.to raise_error(ImpossibleLengthError) 
+      end
+      it 'should throw `InvalidTypeError` if any of the of sides is not a number.' do
+        triangle = Triangle.new 'not', 'an', 'integer'
+        
+        expect {triangle.kind}.to raise_error(InvalidTypeError)
+      end
     end
   end
 end
