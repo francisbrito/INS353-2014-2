c08f3c762308912654d86a794fba2e5fa6b1c244
Francis Brito <fr.br94@gmail.com>
2014-05-15 21:41:29 -0400

    Add specifications for valid sides.

--- class01/src/spec/triangle_spec.rb
+++ class01/src/spec/triangle_spec.rb
@@ -4,4 +4,11 @@ class Triangle
 end
 
 describe Triangle do
+    describe '#kind' do
+        context 'with valid sides' do
+            it 'should return `:equilateral` if all sides are equal length.'
+            it 'should return `:isosceles` if two sides are equal length.'
+            it 'should return `:scalene` if all sides are different length.'
+        end
+    end
 end
