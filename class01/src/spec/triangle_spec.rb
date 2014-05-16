d92ab74b2169b4fae46a2e4dc6a805b6df8ce6e7
Francis Brito <fr.br94@gmail.com>
2014-05-15 21:54:12 -0400

    Implement minimum required code to make  context test fail.

--- class01/src/spec/triangle_spec.rb
+++ class01/src/spec/triangle_spec.rb
@@ -1,14 +1,36 @@
 require 'spec_helper'
 
 class Triangle
+    attr_reader :a, :b, :c
+
+    def initialize(a, b, c)
+        @a = a
+        @b = b
+        @c = c
+    end
+
+    def kind
+    end
 end
 
 describe Triangle do
     describe '#kind' do
         context 'with valid sides' do
-            it 'should return `:equilateral` if all sides are equal length.'
-            it 'should return `:isosceles` if two sides are equal length.'
-            it 'should return `:scalene` if all sides are different length.'
+            it 'should return `:equilateral` if all sides are equal length.' do
+                triangle = Triangle.new 1, 1, 1
+
+                expect(triangle.kind).to eq :equilateral
+            end
+            it 'should return `:isosceles` if two sides are equal length.' do
+                triangle = Triangle.new 1, 1, 2
+
+                expect(triangle.kind).to eq :isosceles
+            end
+            it 'should return `:scalene` if all sides are different length.' do
+                triangle = Triangle.new 1, 2, 3
+
+                expect(triangle.kind).to eq :scalene
+            end
         end
         context 'with invalid sides' do
             it 'should throw `TriangleError` if any side is not a number.'
