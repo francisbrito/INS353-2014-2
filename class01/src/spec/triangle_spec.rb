541ff804ebf6a55ccdd9179cca461f6be6c870fa
Francis Brito <fr.br94@gmail.com>
2014-05-15 21:47:07 -0400

    Add specifications for invalid sides.

--- class01/src/spec/triangle_spec.rb
+++ class01/src/spec/triangle_spec.rb
@@ -10,5 +10,10 @@ describe Triangle do
             it 'should return `:isosceles` if two sides are equal length.'
             it 'should return `:scalene` if all sides are different length.'
         end
+        context 'with invalid sides' do
+            it 'should throw `TriangleError` if any side is not a number.'
+            it 'should throw `TriangleError` if sides violate triangle inequality.'
+            it 'should throw `TriangleError` if any side is negative or zero.'
+        end
     end
 end
