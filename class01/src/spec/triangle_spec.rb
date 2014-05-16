423b0e230ff584d7ef5802caac3d42b5e7ca1e99
Francis Brito <fr.br94@gmail.com>
2014-05-15 22:08:11 -0400

    Implemented minimum required code to make  context fail.

--- class01/src/spec/triangle_spec.rb
+++ class01/src/spec/triangle_spec.rb
@@ -13,6 +13,9 @@ class Triangle
     end
 end
 
+class TriangleError < RuntimeError
+end
+
 describe Triangle do
     describe '#kind' do
         context 'with valid sides' do
@@ -33,9 +36,26 @@ describe Triangle do
             end
         end
         context 'with invalid sides' do
-            it 'should throw `TriangleError` if any side is not a number.'
-            it 'should throw `TriangleError` if sides violate triangle inequality.'
-            it 'should throw `TriangleError` if any side is negative or zero.'
+            it 'should throw `TriangleError` if any side is not a number.' do
+                triangle = Triangle.new 'not', 'a valid', 'number'
+
+                expect {triangle.kind}.to raise_error TriangleError
+            end
+            it 'should throw `TriangleError` if sides violate triangle inequality.' do
+                # NOTE: The sum of the lengths of any two sides must be greater
+                #       than or equal to the length of the remaining side.
+                triangle = Triangle.new 1, 3, 1
+
+                expect {triangle.kind}.to raise_error TriangleError
+            end
+            it 'should throw `TriangleError` if any side is negative or zero.' do
+                # NOTE: For some reason `Triangle.new -1, -1, -1` raise a warning.
+                pessimist_triangle = Triangle.new(-1, -1, -1)
+                expect {triangle.kind}.to raise_error TriangleError
+
+                neutral_triangle = Triangle.new 0, 0, 0
+                expect {triangle.kind}.to raise_error TriangleError
+            end
         end
     end
 end
