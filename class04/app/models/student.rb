f3d03801ea91a60c8a44cecc154767f43ec64538
Francis Brito <fr.br94@gmail.com>
2014-07-10 11:13:15 -0400

    Add get average grade score method to students model.

--- class04/app/models/student.rb
+++ class04/app/models/student.rb
@@ -1,3 +1,13 @@
 class Student < ActiveRecord::Base
     has_many :grades
+
+    def average_grade_score
+        if grades.length == 0
+            0
+        else
+            scores = grades.map {|g| g.score}
+
+            scores.reduce {|p, n| (p + n) / scores.length}
+        end
+    end
 end
